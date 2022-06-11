d0063579eab5f35704233a3bd19205e494191fb4
Adolfo <aapc972@gmail.com>
2022-06-11 16:36:05 -0400

    uibuild

--- index.js
+++ index.js
@@ -37,15 +37,19 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-  if (body.content === undefined) return response.status(400).json({ error: 'content missing' })
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   })
-  note.save().then(savedNote => response.json(savedNote))
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFomattedNote => response.json(savedAndFomattedNote))
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -69,6 +73,8 @@ app.use((error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 })
